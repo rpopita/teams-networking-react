f601394cbc389f8e78e637fc3efdc43ce9eb28c3
rpopita <robert.popita@linnify.com>
2023-08-10 12:09:09 +0300

    map teams to html rows

---
+++ src/footer/component.tsx
@@ -0,0 +1,3 @@
+export function AppFooter() {
+  return <footer>📃 API Docs | 👨‍💻 Source Code</footer>;
+}
---
+++ src/header/index.tsx
@@ -0,0 +1,16 @@
+import logo from "../images/network-team-icon.png";
+import "./style.css";
+
+export default function AppHeader() {
+  return (
+    <header>
+      <div id="header-wrapper">
+        <img src={logo} alt="" width="100" height="100" />
+        <div>
+          <h1>Teams Networking</h1>
+          <h2>CRUD operations example (Create, Read, Update, Delete)</h2>
+        </div>
+      </div>
+    </header>
+  );
+}
